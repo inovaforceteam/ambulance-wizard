c0e05ebf6568a35a9b6fb78a262cbe4aa0f51579
Sule Demir <sule.sozturk@hotmail.com>
2024-08-10 01:50:09 +0300

    FEAT:path controled

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,13 @@
-// next.config.mjs
-
 export default {
     images: {
       remotePatterns: [
         {
           protocol: 'https',
           hostname: 'infinitychassis.com',
-          port: '',
-          pathname: '/wp-content/uploads/**',
+          
         },
       ],
     },
-    // Diğer konfigürasyonlar burada yer alabilir
+
   }
   
