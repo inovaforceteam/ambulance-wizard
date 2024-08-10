1010ef81b63526bddd97ed953666f1c68f94d499
Sule Demir <sule.sozturk@hotmail.com>
2024-08-10 14:51:53 +0300

    FEAT:Some changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={'inter.className d-flex flex-column'}>
+      <body className={'inter.className d-flex flex-column  h-screen'}>
         {children}
         <Footer />        
         </body>
